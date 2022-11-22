0b7895996e60eabfed00204d38f4397c63248769
Czaqq <117601088+Czaqq@users.noreply.github.com>
2022-11-22 18:10:49 +0100

    Update simulations.py

--- simulations.py
+++ simulations.py
@@ -7,18 +7,17 @@ Gdzie najdalej udało się dojść podczas 10_000 spacerów o długości 10_000 
 from random_walks import walk_steps, walk_distance
 
 def simulate_drunk_walks_distance(how_many_walks):
-    for x in range(0, how_many_walks):
-        lowest_step_rate = 0
-        walk_distance
-    return lowest_step_rate
+    step_rate = []
+    distance = how_many_walks
+    for x in range(how_many_walks):
+        step_rate.append(walk_distance(distance))
+    return min(step_rate)
+#print(simulate_drunk_walks_distance(100))
 
-def simulate_drunk_walks_steps(how_many_steps):
-    walk_steps(10)
-
-    steps = []
-    walk_steps
-    return
-
-
-print(simulate_drunk_walks_distance(10_000))
-print(simulate_drunk_walks_steps(10_000))
+def simulate_drunk_walks_steps(how_many_walks):
+    step_rate = []
+    how_many_steps = how_many_walks
+    for x in range(how_many_walks):
+        step_rate.append(walk_steps(how_many_steps))
+    return max(step_rate)
+#print(simulate_drunk_walks_steps(10_000))
